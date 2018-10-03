ef4bd05d2dca5816776a32f0e25648d4d06984a2
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-10-03 22:18:26 +0300

    project structure refactoring

--- microservices-pet-project/sample-client/src/main/kotlin/zhi/yest/sampleclient/SampleClientApplication.kt
+++ microservices-pet-project/sample-client/src/main/kotlin/zhi/yest/sampleclient/SampleClientApplication.kt
@@ -2,13 +2,15 @@ package zhi.yest.sampleclient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
+@EnableEurekaClient
 @Controller
 class SampleClientApplication {
-    @GetMapping("/home")
+    @GetMapping("/")
     fun home(): String {
         return "home"
     }
